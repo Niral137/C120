764446620c8304909ebe54ba2ab8b7f86f052134
Niral137 <88650779+Niral137@users.noreply.github.com>
2023-06-03 10:16:15 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+
+function modelLoaded(){
+  console.log('Model Loaded!');
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  classifier.clssify(video, gotResult);
+}
+var previous_result = ""
+
+
+
